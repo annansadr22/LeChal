1ec1971c3352710c10cd17ba844c2e88b3948077
annansadr22 <126930908+annansadr22@users.noreply.github.com>
2023-03-09 02:12:59 -0500

    added url for initial

--- HireApex/urls.py
+++ HireApex/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 #from django.conf.urls import url
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='dashboard/home.html'),name='home'),
     path('admin/', admin.site.urls),
     path('home/', include('land.urls')),
     #path('home/', TemplateView.as_view(template_name='dashboard/home.html'),name='home'),
